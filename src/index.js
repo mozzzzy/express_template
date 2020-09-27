0cdf44e03e5127295cd1cc0e65a267d39aa5f610
mozzzzy <kurt.goedel.1109@gmail.com>
2020-09-27 14:58:48 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,103 @@
+/*
+ * Module Dependencies
+ */
+
+// External modules
+const express = require('express');
+const log4js = require('log4js');
+
+// Internal modules
+const sleepApp = require('./middlewares/sleep');
+
+/*
+ * Variables
+ */
+
+const logLevel = 'debug';
+const port = process.env.LISTEN_PORT || 3000;
+const shutdowningFlagName = 'shutdowning';
+const serverLogFilePath = 'server.log';
+const accessLogFilePath = 'access.log';
+
+/*
+ * Main
+ */
+
+// Initialize logger
+log4js.configure({
+  appenders: {
+    out: { type: 'stdout' },
+    server: { type: 'file', filename: serverLogFilePath },
+    access: { type: 'file', filename: accessLogFilePath },
+  },
+  categories: {
+    default: {
+      appenders: ['out'],
+      level: logLevel
+    },
+    server: {
+      appenders: ['server', 'out'],
+      level: logLevel
+    },
+    access: {
+      appenders: ['access', 'out'],
+      level: logLevel,
+      layout: {
+        type: 'pattern',
+        pattern: '%d %m',
+      }
+    },
+  }
+});
+const serverLogger = log4js.getLogger('server');
+const accessLogger = log4js.getLogger('access');
+
+// Initialize app
+const app = express();
+app.set(shutdowningFlagName, false);
+
+// When shutdowning flag is true, set "Connection: close" header to response.
+app.use((req, res, next) => {
+  const shutdowningFlag = app.get(shutdowningFlagName);
+  serverLogger.debug(`Check shutdowning flag. Shutdowning flag is \"${shutdowningFlag}\".`);
+  if (shutdowningFlag) {
+    serverLogger.debug('Set Connection close header to response.');
+    res.set('Connection', 'close');
+  }
+  next();
+});
+
+app.get('/sleep', sleepApp);
+
+// When app sent the http response and it has "Connection: close" header,
+// close tcp connection.
+app.use((req, res, next) => {
+  const connectionHeader = res.get('Connection');
+  if (res.headersSent && connectionHeader === 'close') {
+    serverLogger.debug(`Server sent Connection close header. Close tcp connection.`);
+    res.connection.destroy();
+  }
+  next();
+});
+
+// Log access
+app.use((req, res) => {
+  accessLogger.info(`${req.hostname} ${req.method} ${req.originalUrl} ${res.statusCode}`);
+});
+
+// Listen
+const server = app.listen(port, () => {
+  serverLogger.info(`Server is listening at ${port} port.`);
+});
+
+// Handle signals
+process.on('SIGTERM', () => {
+  serverLogger.debug('Process got SIGTERM.');
+
+  serverLogger.debug('Set shutdowning flag true.');
+  app.set(shutdowningFlagName, true);
+
+  server.close(() => {
+    serverLogger.debug('Server closed all tcp connections.');
+  });
+});
---
+++ src/middlewares/sleep.js
@@ -0,0 +1,30 @@
+// External modules
+const express = require('express');
+const log4js = require('log4js');
+
+// Get logger
+const serverLogger = log4js.getLogger('server');
+
+const app = express();
+app.get('/sleep', (req, res, next) => {
+  const sleepTimeMsStr = req.query.ms;
+  if (sleepTimeMsStr === undefined) {
+    res.status(400).send('Query ms=<milli second> is required.');
+    next();
+    return;
+  }
+  sleepTimeMs = parseInt(sleepTimeMsStr, 10);
+  if (Number.isNaN(sleepTimeMs)) {
+    res.status(400).send(`Invalid query ms=\"${sleepTimeMsStr}\".`);
+    next();
+    return;
+  }
+
+  serverLogger.debug(`Sleep ${sleepTimeMs} milli seconds and return response.`);
+  setTimeout(() => {
+    res.status(200).send('OK');
+    next();
+  }, sleepTimeMs);
+});
+
+module.exports = app;
