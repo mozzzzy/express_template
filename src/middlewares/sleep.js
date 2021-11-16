80a724aac8c2a8bb543c4d7a67dcd69c1ae830b2
mozzzzy <kurt.goedel.1109@gmail.com>
2021-11-16 18:29:05 +0900

    refactor: src/middlewares/sleep.js

--- src/middlewares/sleep.js
+++ src/middlewares/sleep.js
@@ -5,29 +5,35 @@ const log4js = require('log4js');
 // Get logger
 const serverLogger = log4js.getLogger('server');
 
+const buildBody = msg => {
+  return {
+    msg
+  };
+};
+
 const app = express();
 app.get('/sleep', (req, res, next) => {
   const sleepTimeMsStr = req.query.ms;
   if (sleepTimeMsStr === undefined) {
-    res.status(400).send('Query ms=<milli second> is required.');
+    res.status(400).json(buildBody('Query ms=<milli second> is required.'));
     next();
     return;
   }
   const sleepTimeMs = parseInt(sleepTimeMsStr, 10);
   if (Number.isNaN(sleepTimeMs)) {
-    res.status(400).send(`Invalid query ms="${sleepTimeMsStr}".`);
+    res.status(400).json(buildBody(`Invalid query ms="${sleepTimeMsStr}".`));
     next();
     return;
   }
   if (!Number.isSafeInteger(sleepTimeMs)) {
-    res.status(400).send(`Query ms="${sleepTimeMsStr}" is too large.`);
+    res.status(400).send(buildBody(`Query ms="${sleepTimeMsStr}" is too large.`));
     next();
     return;
   }
 
   serverLogger.debug(`Sleep ${sleepTimeMs} milli seconds and return response.`);
   setTimeout(() => {
-    res.status(200).send('OK');
+    res.status(200).json(buildBody('OK'));
     next();
   }, sleepTimeMs);
 });
