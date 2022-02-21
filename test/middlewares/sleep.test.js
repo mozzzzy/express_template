2c7cbcb8d6ffd0ae53ce8df2c7408915b9fbb3d8
mozzzzy <kurt.goedel.1109@gmail.com>
2022-02-21 13:36:39 +0900

    refactor/test: use async/await in sleep.test.js

--- test/middlewares/sleep.test.js
+++ test/middlewares/sleep.test.js
@@ -2,19 +2,27 @@ const request = require('supertest');
 const app = require('../../src/middlewares/sleep');
 
 describe('Test of sleep module', () => {
-  test('Return 200 OK', () => request(app).get('/sleep?ms=1000').then((res) => {
+  test('Return 200 OK when request with valid ms query', async () => {
+    const beforeRequest = new Date();
+    const res = await request(app).get('/sleep?ms=1000');
+    const afterRequest = new Date();
+
+    expect(afterRequest.getTime() - beforeRequest.getTime()).toBeGreaterThan(1000);
     expect(res.statusCode).toBe(200);
-  }));
+  });
 
-  test('Return 400 Bad Request when request without ms query', () => request(app).get('/sleep').then((res) => {
+  test('Return 400 Bad Request when request without ms query', async () => {
+    const res = await request(app).get('/sleep');
     expect(res.statusCode).toBe(400);
-  }));
+  });
 
-  test('Return 400 Bad Request when request with ms=string query', () => request(app).get('/sleep?ms=string').then((res) => {
+  test('Return 400 Bad Request when request with ms=string query', async () => {
+    const res = await request(app).get('/sleep?ms=string');
     expect(res.statusCode).toBe(400);
-  }));
+  });
 
-  test('Return 400 Bad Request when request with too big ms query', () => request(app).get('/sleep?ms=100000000000000000').then((res) => {
+  test('Return 400 Bad Request when request with too big ms query', async () => {
+    const res = await request(app).get('/sleep?ms=100000000000000000');
     expect(res.statusCode).toBe(400);
-  }));
+  });
 });
