736954fbc618368e17589e07766353c9fb1d3dee
mozzzzy <kurt.goedel.1109@gmail.com>
2022-02-21 13:40:05 +0900

    style: Fix style of eslintrc.js

--- eslintrc.js
+++ eslintrc.js
@@ -7,8 +7,9 @@ module.exports = {
   ],
   rules: {
     // I disable no-continue rule.
-    // 'continue' statement is usable for me to eary return.
-    // 'no-continue' rule prevent me from do eary return and make me write long codes in a if block.
+    // 'continue' statement is usable for me to early return.
+    // 'no-continue' rule prevent me from do early return and
+    // make me write long codes in a if block.
     'no-continue': 'off',
   },
 };
